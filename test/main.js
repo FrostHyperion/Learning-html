46748902b1fa2213406009b0f934c9e7099fc688
FrostHyperion <sahilnarwal1032@gmail.com>
2020-04-26 03:19:23 +0530

    refactoring the app

---
+++ test/main.js
@@ -0,0 +1,11 @@
+function test()
+{
+result="Button Clicked!!!!!!!"
+var place = document.getElementById("test");
+place.innerHTML = result;
+}
+function color()
+{
+    document.getElementById('id1').style.color = 'red'
+
+}
